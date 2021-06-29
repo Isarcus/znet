7ede383fa4a59194147135647246b883854e5bfb
Isaac Brooks <iabrooks@mtu.edu>
2021-06-29 08:55:11 -0400

    Fixed RNG in main

--- main.cpp
+++ main.cpp
@@ -14,27 +14,31 @@ void print(dataset_t data);
 
 int main(int argc, char** argv)
 {
-    Network nw(2, 2, {5});
+    Network nw(2, 2, {5, 5});
 
     nw.randomizeWeights();
-    dataset_t input{0, 0};
+    dataset_t input;
 
+    input = {0, 0};
     print(nw.process(input));
-    for (int i = 0; i < 10000; i++)
+    std::cout << "\n";
+    for (int i = 0; i < 200000; i++)
     {
         input[0] = i % 2;
         input[1] = (i + 1) % 2;
 
-        nw.train(input, {1, 0}, 0.1);
+        nw.train(input, dataset_t{input[1], input[0]}, 0.0009);
     }
+    //nw.printAll();
+    input = {1, 0};
     print(nw.process(input));
 }
 
 double rand(double min, double max)
 {
-    bool calledYet = false;
-
-    std::default_random_engine eng;
+    static bool calledYet = false;
+    static std::default_random_engine eng;
+    
     if (!calledYet)
     {
         eng.seed(std::chrono::system_clock::now().time_since_epoch().count());
